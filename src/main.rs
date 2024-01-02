f8c49658f261ccacbbdd663d4b6667d6e49e43fd
Stig Nervik <stig.nervik@carrier.com>
2024-01-02 08:22:48 +0100

    Implement support for storing todos to file.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use clap::{Parser, Subcommand};
+use serde::ser::SerializeMap;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
+use std::path::Path;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -46,19 +48,37 @@ impl ToDos {
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut todos = ToDos::new();
 
     let args = Args::parse();
     println!("Commands {:?}", args);
+
+    // Check first if the file exist, if so read the todos and store it.
+    let file_path = "todos.yaml";
+    let file_exist = Path::new(file_path).exists();
+    let values: Vec<String> = if file_exist {
+        let mut file = File::open(file_path)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        serde_yaml::from_str(&contents)?
+    } else {
+        Vec::new()
+    };
+
+    todos.store.extend(values);
 
     match &args.cmd {
         Commands::Create { name } => {
             println!("Created a new ToDo: {:?}", name);
             if let Some(name) = name {
                 todos.push(name.to_string());
+
+                let serialized = serde_yaml::to_string(&todos.store);
+                let mut file = File::create(file_path)?;
+                file.write_all(serialized.as_bytes())?;
             }
-        }
+        }d
         Commands::Read { name } => {
             println!("Read ToDo: {:?}", name);
             todos.print();
@@ -70,4 +90,6 @@ fn main() {
             println!("Delete ToDo with name {:?}", name);
         }
     }
+
+    Ok(())
 }
